4e3ee4afdd11c68dabe4fa1f7a3c966bbb5701d8
Artem <nimoshka@gmail.com>
2022-05-10 17:48:59 +0300

    Add loyaut

---
+++ index.js
@@ -0,0 +1,19 @@
+function operate(number1, number2, operator){
+
+}
+
+function add(number1, number2){
+    
+}
+
+function subtract(number1, number2){
+    
+}
+
+function multiply(number1, number2){
+    
+}
+
+function divide(number1, number2){
+    
+}
