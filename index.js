105feaf3f06139e98710414e073574b8d25b7d9a
Artem <nimoshka@gmail.com>
2022-05-11 20:11:33 +0300

    Set some operator(!=) and number(!.)  functions, cancel, animation, display refresh

--- index.js
+++ index.js
@@ -1,41 +1,143 @@
-let number1 = [];
-let number2 = [];
-let currentOperator, topLine, bottomLine;
+let topLine = [];
+let bottomLine = [];
+let operator, number1, number2, lastPushedButton;
 
-// function operate(number1, number2, operator) {
 
-// }
+function operate(number1, number2, operator) {
+    let result;
 
-// function add(number1, number2) {
+    switch (operator) {
+        case '+':
+            result = number1 + number2;
+            break;
+        case '-':
+            result = number1 - number2;
+            break;
+        case '*':
+            result = number1 * number2;
+            break;
+        case '/':
+            result = number1 / number2;
+            break;
+    }
+    console.log(result);
+    return result.toString().split('');
+};
 
-// }
 
-// function subtract(number1, number2) {
-
-// }
-
-// function multiply(number1, number2) {
-
-// }
-
-// function divide(number1, number2) {
-
-// }
+//Numbers or point clicked
 
 function clickNumber() {
+
+    buttonAnimation(this);
+    bottomLine.push(this.textContent);
+    refreshBottomLine();
+    lastPushedButton = this.textContent;
+};
+
+
+function point() {
     console.log(this.textContent);
+
 }
+
+
+//Operators or Cancel clicked
 
 function clickOperator() {
-    console.log(this.textContent);
+    buttonAnimation(this);
+
+    if (operator == undefined) {
+        firstOperator(this.textContent);
+    } else {
+        secondaryOperator(this.textContent);
+    }
+};
+
+
+function firstOperator(lastOperator) {
+
+    if(lastPushedButton == undefined){
+        return;
+    }
+
+    topLine = [...bottomLine];
+    topLine.push(' ', lastOperator);
+    lastPushedButton = lastOperator;
+    operator = lastOperator;
+    number1 = +bottomLine.join('')
+    console.log('n1 = ' + number1);
+    refreshTopLine();
+    bottomLine = [];
 }
 
-function cancel() {
-    console.log(this.textContent);
+
+function secondaryOperator(lastOperator) {
+
+    if (lastPushedButton == /[/\-+=*]/) {
+
+        lastPushedButton = lastOperator;
+        topLine.pop().push(lastOperator);
+
+    } else {
+
+        lastPushedButton = lastOperator;
+        number2 = +bottomLine.join('');
+        bottomLine = [...operate(number1, number2, operator)];
+        topLine = [...bottomLine];
+        topLine.push(' ', lastOperator);
+        number1 = +bottomLine.join('');
+        refreshBottomLine();
+        bottomLine = [];
+    }
+
+    operator = lastOperator;
+    refreshTopLine();
 }
+
+
+function cancel() {
+    topLine = [];
+    bottomLine = [];
+    lastPushedButton = undefined;
+    operator = undefined;
+    number1 = undefined;
+    number2 = undefined;
+    refreshBottomLine();
+    refreshTopLine();
+};
+
+
+function equals(lastOperator) {
+    
+
+}
+
+
+//Display and animation
+
+function refreshTopLine() {
+    document.querySelector('.top_line').textContent = topLine.join('');
+};
+
+
+function refreshBottomLine() {
+    document.querySelector('.bottom_line').textContent = bottomLine.join('');
+};
+
+
+function buttonAnimation(button){
+    button.classList.add('pressed');
+    setTimeout(() => {
+        button.classList.remove('pressed');
+    }, 100);
+}
+
 
 //Event Listeners
 
 document.querySelectorAll('.button_number').forEach(e => e.addEventListener('click', clickNumber));
 document.querySelectorAll('.button_operator').forEach(e => e.addEventListener('click', clickOperator));
 document.querySelector('.cancel').addEventListener('click', cancel);
+document.querySelector('.point').addEventListener('click', point);
+document.querySelector('.equals').addEventListener('click', equals);
